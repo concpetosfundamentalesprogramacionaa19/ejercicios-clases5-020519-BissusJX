4cb8fd4b5a101fe8c043eee0f89710f5ca85a7c3
BissusJX <bissus17@gmail.com>
2019-05-02 10:41:35 -0500

    pythonEjercicioClase05

---
+++ miproyecto/run4.py
@@ -0,0 +1,29 @@
+"""
+   deseamos optener el costo de una carrera universitaria.
+   El valor promedio de cada ciclo es de 1200$, el valor promedio del seguro educativo por ciclo es de 100$
+   si la edad del estudiante es menor o igual a 20 caso contrario sera de 150$ si el estudiante 
+   tiene una modalidad a distancia el numero de ciclos a seguir es de 10 caso contrario debera seguir 8 ciclos 
+   obtener la proyeccion del costo de la carrera de un estudiante.
+"""
+valorPromedio = 1200
+#seguro = 100 
+#seguro2 = 150
+edad = input("ingrese la edad del estudiante\n")
+modalidad = input ("ingrese la modalidad del estudiante\n")
+#ciclo = input ("ingrese el ciclo en que se encuentra el estudiante\n")
+edad = int (edad)
+seguro = int (seguro)
+seguro2 = int (seguro2)
+if (edad<=20)
+     seguro = 100
+   print ("el valor promedio del seguro por ciclo es de %d", seguro)
+   else:
+   	if (edad>20)
+   	     seguro = 150
+   	   print ("el valor promedio del seguro por ciclo es de %d", seguro)
+if (modalidad == distancia)
+   ciclo = 10
+   else:
+   	ciclo = 8 
+   	totalSeguro = ciclo * (seguro* 1200)
+
